18edf7193aed81c9608b1168c0adc4ad0599c140
matssaaren <167091453+matssaaren@users.noreply.github.com>
2025-06-10 18:35:26 +0300

    Redesign site with navigation and theme

--- src/index.js
+++ src/index.js
@@ -8,19 +8,21 @@ import {
 } from 'react-router-dom'
 
 import './style.css'
-import Galery from './views/galery'
+import Gallery from './views/galery'
 import Info from './views/info'
 import Contact from './views/contact'
 import Home from './views/home'
 import NotFound from './views/not-found'
+import Hifi from './views/hifi'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route component={Galery} exact path="/galery" />
+          <Route component={Gallery} exact path="/galery" />
         <Route component={Info} exact path="/info" />
         <Route component={Contact} exact path="/contact" />
+        <Route component={Hifi} exact path="/hifi" />
         <Route component={Home} exact path="/" />
         <Route component={NotFound} path="**" />
         <Redirect to="**" />
